86aebbc1006d41822964b79dc0c592c1e3cdcb84
Nour-mnassri <nourmnassry27@gmail.com>
2023-12-20 22:30:08 +0100

    Mohamed Portfolio

--- src/Details.js
+++ src/Details.js
@@ -3,17 +3,16 @@
 import logogradient from "./assets/logo.svg";
 import logo from "./assets/logo2.svg";
 // Profile Image
-import profile from "./assets/profile.png";
+import nouur from "./assets/nouur.jpg";
 // Tech stack images
-import html from "./assets/techstack/html.png";
-import css from "./assets/techstack/css.png";
-import js from "./assets/techstack/js.png";
-import react from "./assets/techstack/react.png";
-import bootstrap from "./assets/techstack/bootstrap.png";
+import cprog from "./assets/cprog.png";
+import Linux from "./assets/Linux.png";
+import Python from "./assets/python.png";
+import matlab from "./assets/matlab.png";
 import vscode from "./assets/techstack/vscode.png";
 import github from "./assets/techstack/github.png";
 import git from "./assets/techstack/git.png";
-import npm from "./assets/techstack/npm.png";
+
 
 
 // Logos
@@ -24,95 +23,361 @@ export const logos = {
 
 // Enter your Personal Details here
 export const personalDetails = {
-  name: "Mnassri Nour",
-  tagline: "Web developer",
-  img: profile,
-  about: `I am a web developer, I have a passion for web development and love to create for web and mobile devices. I am a hard worker and I am always looking for new challenges to improve my skills.`,
+  name: "Brahmi Mohamed",
+  tagline: "Phd Engineer Autonomous Driving",
+  img: nouur,
+  about: `Broad spectrum of knowledge in Advanted Driver Assistance systems with focus on radar based ADAS,validation and simultation/Reprocessing.acts as knowledge multiplier and key player in international project environement.
+  `,
 };
 
 // Enter your Social Media URLs here
 export const socialMediaUrl = {
-  linkdein: "https://www.linkedin.com/",
-  github: "https://github.com/Nour-mnassri",
-  twitter: "https://twitter.com/",
-  instagram: "https://www.instagram.com/nour_mnassry27/",
+  linkdein: "https://www.linkedin.com/in/mohamed-brahmi-009638b1/?originalSubdomain=de",
+  // github: "https://",
+  // twitter: "https://twitter.com/",
+  // instagram: "https://instagram.com%2Fhamma_brah",
 };
 
-// Enter your Work Experience here
+// DA5LY EXPERIENCE HNA
 export const workDetails = [
 
-
+//EXPERIENCE
   {
-    Position: "Internship",
-    Company: `Zitoona Bank`,
-    Location: "Gafsa",
-    Type: "Internship",
-    Duration: "Jan 2021 - Fev 2021",
+    Position: "Dr-Ing",
+    Company: `BMW GROUP`,
+    Location: "Munich,Baviére,Allemagne",
+    Type: "BMW,Technical Product Management Autonomous Driving Virtual Validation",
   },
+
+//EXPERIENCE
+  {
+    Position: "Cluster Product Owner Virtual Validation L3 Autonomous Driving",
+    Company: `BMW GROUP`,
+    Location: "München, Bayern, Deutschland • Hybride",
+    Type: "Responsable for Validation methods, tools & concepts for the data driven development",
+    Duration: " mai 2023 aujourd'hui 8 mois    ",
+    Skills:"Agiles Projektmanagement Technisches Produktmanagement • Validation • Advanced driver assistance systems • Automotive Radar • Produktentwicklung"
+  },
+
+//EXPERIENCE 
+  {
+    Position: "Product Owner: Virtual Validation Autonomous Driving ",
+    Company: `BMW GROUP`,
+    Location: " München, Bayern",
+    Type: "Product Owner Reprocessing of Sensor Data with Focus on Validation for the purpose of Virtual Validation of automated driving functions.",
+    Duration: "2020 - nov. 2023 • 3 ans 8 mois",
+    Skills: "C++ python (Programmiersprache) Apache Airflow • Validation • Ethernet"
+  },
+
+//EXPERIENCE 
+  {
+    Position: "Lecturer Automated Driving & ADAS (Parttime)",
+    Company: `BMW GROUP`,
+    Location: " München, Bayern, Deutschland",
+    Type: "Introduce Master students to the Field of AD and ADAS by combining theoretical and practical aspects from the industry about Sensors, Environmental Perception, Product development and Validation.",
+    Duration: "Hochschule München • Temps partiel mars 2022 - aujourd'hui • 1 an 10 mois",
+  },
+  //EXPERIENCE 
+  {
+    Position: "Head of Core Radar System Requirements and Architecture",
+    Company: `Continental`,
+    Location: " München und Umgebung, Deutschland",
+    Type: "Head of Core Development Radar System Requirements and Architecture bei Continental",
+    Duration: "Oct. 2019 - mars 2020 • 6 mois",
+  },
+  //EXPERIENCE 
+  {
+    Position: "Teamleader Front Radar System Engineering ADAS ",
+    Company: `Continental`,
+    Location: " Yokohama, Kanagawa, Japan",
+   Duration: "avr. 2018 - sept. 2019 • 1 an 6 mois ",
+  },
+   //EXPERIENCE 
+   {
+    Position: "ADAS Systems Engineering, Tech. Lead & Mentoring ",
+    Company: `Continental`,
+    Location: " Yokohama, Kanagawa, Japan",
+    Type: "Coaching and training for new system engineers in product development process and radar technology. Coordination of Project transfer from overseas location to Yokohama location (According to the new strategy from the region to the region)",
+    Duration: "aoüt 2016 - avr. 2018 • 1 an 9 mois",
+  },
+    //EXPERIENCE 
+    {
+      Position: "ADAS Systems Engineer ",
+      Company: `Continental`,
+      Location: " Yokohama, Kanagawa, Japan",
+      Type: "System Engineering, Requirements Engineering, System Design, Interface between customer and internal departments (SW HW, Test and Validation) ASPICE,supporting Industrialisation  and manufacturing activities.",
+      Duration: "avr. 2014 -juil. 2016 • 2 ans 4 mois",
+    },
+     //EXPERIENCE 
+     {
+      Position: "Assistant lecturer ",
+      Company: `Technische Universität `,
+      Location: " München • CDD",
+      Type: "Betreuung der Vorlesung Fahrerassistenzsysterne in kfz im Rahmen der Promotion bei Audi AG und TIJ-BS",
+      Duration: "avr. 2011 - mars 2014 3ans",
+    },
+     //EXPERIENCE 
+     {
+      Position: "Research Engineer ",
+      Company: `TU Braunschweig/AUDl AG `,
+      Location: " München • CDD",
+      Type: "Reference Systems for Evironmental Perception",
+      Duration: "juil. 2010-févr. 2014-3 ans 8 mois",
+    },
+  
+  
+
+
 ];
 
-// Enter your Education Details here
+//  EDUCATION 
 export const eduDetails = [
+  // EDUCATION
   {
-    Position: "Bachelor's Degree",
-    Company: "ISET Gafsa",
-    Location: "Gafsa",
-    Type: "Full Time",
-    Duration: "2021 - Present",
+    Company: "Technical university of munich",
+    Location: "munich germany",
+    Type: "Diplom-Ingenieur,Elektrotechnik, Eletronik und Kommunikationstechnik",
+    Duration: "2003-2010",
+  },
+  {
+    Company: "Technische Universität Braunschweig",
+    Location: "Brunswick Allemagne",
+    Type: "Dr.Ing, Elektrotechnik und Elektronik",
+    Duration: "2014-2019",
+  },
+  {
+    Company: "Centrale Lille",
+    Location: "Lille France",
+    Type: "Master of Science (MSc), Ingenieurwissenschaften",
+    Duration: "2005-2007",
+  },
+  {
+    Company: "Lycée Pilote de Sousse",
+    Location: "Sousse Tunisie",
+    Type: "Baccalauréat",
+    Duration: "2000-2002",
   },
 
+
+ 
 ];
 
 // Tech Stack and Tools
 export const techStackDetails = {
-  html: html,
-  css: css,
-  js: js,
-  react: react,
-  bootstrap: bootstrap,
+   cprog:cprog,
+   Linux:Linux,
+   matlab: matlab,
+   Python: Python,
   vscode: vscode,
-  npm: npm,
   git: git,
-  github: github,
+  github: github
 
 };
 
 // Enter your Project Details here
 export const projectDetails = [
-  {
-    title: "E-commerce Website",
-    image: "projectImage1",
-    description: `Developed a full-fledged e-commerce website with features like user authentication, 
-        product catalog, shopping cart, and secure payment processing. Implemented responsive design 
-        for seamless user experience across devices.`,
-    techstack: "React, Node.js, Express, MongoDB",
-    previewLink: "#",
-    githubLink: "https://github.com/",
+  
+
+{
+  title: "Agile Project Management",
+
+   description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
 },
 {
-  title: "Contact Manager App",
-  image: "contactManagerAppImage",
-  description: `Developed a contact manager application to organize and manage contacts efficiently. 
-      Implemented features such as adding new contacts, editing existing ones, and deleting contacts. 
-      Used local storage for data persistence and provided a clean and intuitive user interface.`,
-  techstack: "Angular, Bootstrap",
-  previewLink: "#",
-  githubLink: "https://github.com/example-contact-manager",
+  title: "BMW Technical Product Management",
+
+  description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,
+    // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
 },
 {
-  title: "Task Tracker App",
-  image: "taskTrackerAppImage", 
-  description: `Designed and implemented a task tracker application to help users manage their tasks efficiently. 
-      The app includes features like adding tasks, setting priorities, marking tasks as completed, and 
-      organizing tasks by category. Integrated a clean and responsive user interface for an optimal user experience.`,
-  techstack: "Vue.js, Vuex, SCSS, Firebase",
-  previewLink: "#",
-  githubLink: "https://github.com",
-}
+  title: "C++",
+
+  description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Python (Programming Language)",
+  description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+
+ 
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Apache Airflow",
+
+  description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Validation",
+
+  description: `2 experiences at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Ethernet",
+
+  description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Software development",
+
+  // description: `Broad spectrum of knowledge in Advanted Driver Assistance systems with focus on radar based ADAS,validation and simultation/Reprocessing.acts as knowledge multiplier and key player in international project environement..`,
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Advanced driver assistance systems",
+
+  description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Automotive Radar",
+
+  description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "ASPICE",
+
+ // description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "System Design",
+
+ // description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Requirements Engineering",
+
+ // description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Rhapsody",
+
+ // description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "System integration Test",
+
+ // description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Matlab",
+
+ // description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Autosar",
+
+ // description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Executive management",
+
+ // description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+
+{
+  title: "Management",
+
+ // description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Radar",
+
+ // description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Product development",
+
+  description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+{
+  title: "Embedded systems",
+
+ // description: `Cluster Product Owner Virtual Validation L3 Autonomous Driving at BMW Group`,  // techstack: "React, Node.js, Express, MongoDB",
+  // techstack: "React, Node.js, Express, MongoDB",
+  // previewLink: "#",
+  // githubLink: "https://github.com/",
+},
+// {
+//   title: "Contact Manager App",
+//   image: "contactManagerAppImage",
+//   description: `Developed a contact manager application to organize and manage contacts efficiently. 
+//       Implemented features such as adding new contacts, editing existing ones, and deleting contacts. 
+//       Used local storage for data persistence and provided a clean and intuitive user interface.`,
+//   techstack: "Python, c++",
+//   previewLink: "#",
+//   githubLink: "https://github.com/example-contact-manager",
+// },
+// {
+//   title: "Task Tracker App",
+//   image: "taskTrackerAppImage", 
+//   description: `Designed and implemented a task tracker application to help users manage their tasks efficiently. 
+//       The app includes features like adding tasks, setting priorities, marking tasks as completed, and 
+//       organizing tasks by category. Integrated a clean and responsive user interface for an optimal user experience.`,
+//   techstack: "Vue.js, Vuex, SCSS, Firebase",
+//   previewLink: "#",
+//   githubLink: "https://github.com",
+// }
 ];
 
 // Enter your Contact Details here
 export const contactDetails = {
-  email: "nourmnassry27@gmail.com",
-  phone: "+216 22 22 22 22",
+  email: "mohamed.brahmi@hm.edu",
 };
--- src/Pages/Technologies.js
+++ src/Pages/Technologies.js
@@ -11,7 +11,10 @@ function Technologies() {
     vscode,
     git,
     github,
-    npm,
+    matlab,
+    Python,
+    cprog
+    
   } = techStackDetails;
   return (
     <main className="container mx-auto max-width pt-10 pb-20 ">
@@ -24,11 +27,10 @@ function Technologies() {
         </p>
       </section>
       <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
-        <img src={html} title="html" alt="" />
-        <img src={css} title="CSS" alt="" />
-        <img src={js} title="JavaScript" alt="" />
-        <img src={react} title="React" alt="" />
-        <img src={bootstrap} title="Bootstrap" alt="" />
+        <img src={Python} title="Python" alt="" />
+        <img src={cprog} title="C++" alt="" />
+        <img src={matlab} title="MatLab" alt="" />
+       
       </section>
       <section>
         <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
@@ -39,7 +41,7 @@ function Technologies() {
         <img src={vscode} title="Visual Studio Code" alt="" />
         <img src={git} title="Git" alt="Git" />
         <img src={github} title="Github" alt="Github" />
-        <img src={npm} title="NPM" alt="NPM" />
+       
       </section>
     </main>
   );
